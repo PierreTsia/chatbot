43945b6d788cc4428300e85532699cdc67bd5581
Pierre_T <pierre.tsiakkaros@gmail.com>
2018-01-11 17:46:10 +0100

    debut get

--- client/app/chatcomponent/chatcomponent.component.ts
+++ client/app/chatcomponent/chatcomponent.component.ts
@@ -1,25 +1,42 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';  
-import { ChatService, Message } from '../services/chat.service';  
-import { Observable } from 'rxjs/Observable';  
-import 'rxjs/add/operator/scan';  
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { ChatService, Message } from '../services/chat.service';
+import { Observable } from 'rxjs/Observable';
+
+
+import 'rxjs/add/operator/scan';
 
 @Component({
   selector: 'app-chatcomponent',
   templateUrl: './chatcomponent.component.html',
   styleUrls: ['./chatcomponent.component.scss'],
-  encapsulation: ViewEncapsulation.None  
+  encapsulation: ViewEncapsulation.None
 })
+
 export class ChatcomponentComponent implements OnInit {
-  messages: Observable<Message[]>;  
-  strMsg: string;  
-  constructor(private chat: ChatService) { }  
-  ngOnInit() {  
-    this.messages = this.chat.conversation.asObservable()  
-      .scan((acc, val) => acc.concat(val));  
-  }  
-  sendMessage() {  
-    this.chat.talk(this.strMsg);  
-    this.strMsg = '';  
-  }  
+  messages: Observable<Message[]>;
+  strMsg: string;
+  public supplyOffers = []
+  constructor(private chat: ChatService) { }
+
+
+  ngOnInit() {
+    this.messages = this.chat.conversation.asObservable()
+      .scan((acc, val) => acc.concat(val));
+   this.getSupplyOffers();
+  }
+  sendMessage() {
+    this.chat.talk(this.strMsg);
+    this.strMsg = '';
+  }
+
+  getSupplyOffers(){
+    this.chat.getSupplyOffers().subscribe(
+      data => {
+        this.supplyOffers = data,
+          console.log(this.supplyOffers)
+      },
+      error => console.log(error),
+    );
+  }
 
 }
